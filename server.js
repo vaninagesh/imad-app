96605acfebbacc094779de6aa50c94bf943c7fb7
vaninagesh <vaninagesh4362@gmail.com>
2017-08-09 20:30:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/article-one',function (req,res){
     res.send('Article one requested and it will be served');
 });
+app.get('/article-two',function (req,res){
+    res.send('Article two requested and it will be served');
+});
+app.get('/article-three',function (req,res){
+    res.send('Article three requested and it will be served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
